ae01fa4b2ff428713b2eb226c704b7812e616799
sorryz23 <sorryz23@gmail.com>
2023-03-03 15:31:32 -0500

    finally missing files returned

---
+++ src/components/About/index.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+
+function About() {
+  return (
+    <section className="my-5">
+      <div className="my-2">
+        <div className="profile my-5">
+        
+        </div>
+        <p>
+     
+        </p>
+        <p>
+        Hello, and welcome to my Portfolio! My name is Cody Andrews. I have been into technology for many years now. I have always had a passion for it and decided to pursue a bachelors in Computer Science. I recently changed course and this has lead me to where I am today. I started a Fullstack Bootcamp and will continue to pursue my goal of becoming a web-designer.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
